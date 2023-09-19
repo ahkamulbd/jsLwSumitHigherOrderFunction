26b433960ec433d23d00465bee02341e37a431f0
ahkamulbd <ahkamulbd@gmail.com>
2023-09-19 12:14:38 +0600

    Date Object addressed

---
+++ js/dateObject.js
@@ -0,0 +1,33 @@
+//console.dir(Date);
+
+// console.log(Date());
+// console.log(typeof Date());
+
+const dateObject = new Date();
+
+// console.log(new Date());
+// console.log(typeof new Date());
+
+// console.log(new Date(2014, 0, 3));
+
+// console.dir(new Date());
+
+// console.log(`Get Date: ${dateObject.getDate()}`);
+// console.log(`Get Day: ${dateObject.getDay()}`);
+// console.log(`Get Full Year: ${dateObject.getFullYear()}`);
+// console.log(`Get Hours: ${dateObject.getHours()}`);
+// console.log(`Get Milliseconds: ${dateObject.getMilliseconds()}`);
+// console.log(`Get Minutes: ${dateObject.getMinutes()}`);
+// console.log(`Get Months: ${dateObject.getMonth()}`);
+// console.log(`Get Seconds: ${dateObject.getSeconds()}`);
+// console.log(`Get Year: ${dateObject.getYear()}`);  // Counted from 1900 
+
+
+//console.log(`Set Date: ${dateObject}`);
+
+//dateObject.setMonth(9);
+
+//console.log(`Set Month: ${dateObject}`);
+
+console.log(dateObject.toDateString());
+
